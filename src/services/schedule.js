290e2a3d19d39f1547dd67910209e3de95f4cb13
Nguyễn Công Tuấn <147248673+TOIsMeMan@users.noreply.github.com>
2025-04-14 23:51:23 +0700

    add time filter in get schedule

--- src/services/schedule.js
+++ src/services/schedule.js
@@ -13,65 +13,81 @@ import db from '../models'
         return response? response.id : null
     }
 
-    export const getSchedules = ({fromLocationId, toLocationId}) => new Promise(async (resolve, reject) => {
+    export const getSchedules = ({fromLocationId, toLocationId, date}) => new Promise(async (resolve, reject) => {
         try {
-            const routeId = await getRouteId({fromLocationId: fromLocationId, toLocationId: toLocationId})
-            if (routeId !== null) {
-                const scheduleResponse = await db.Schedule.findAndCountAll({
-                    where: {
-                        routeId: routeId
-                    },
-                    attributes:{
-                        exclude: ['createdAt', 'updatedAt']                            
-                    },
-                    raw: true
-                })
-                const routeResponse = await db.Route.findOne({
-                    where: {
-                        id: routeId
-                    },
-                    attributes: [
-                        'id'
-                    ],
-                    include: [
-                        { 
-                            model: db.Location, 
-                            as: 'fromLocation' ,
-                            attributes: [
-                                'id',
-                                'name',
-                                'province'
-                                
-                            ]
-                        },
-                        { 
-                            model: db.Location, 
-                            as: 'toLocation',
-                            attributes: [
-                                'id',
-                                'name',
-                                'province'
-                                
-                            ] 
-                        }
-                    ],
-                    raw: true,
-                    nest: true
-                })
-                resolve({
-                    success: scheduleResponse ? true : false,
-                    data: {
-                        scheduleData: scheduleResponse? scheduleResponse : null,
-                        routeData: routeResponse? routeResponse : null
-                    }
-                })
-            }
-
-            resolve({
-                success: false,
-                data: null
-            })
             
+            if (fromLocationId && toLocationId) {
+                const routeId = await getRouteId({fromLocationId: fromLocationId, toLocationId: toLocationId})
+                if (routeId !== null) {
+
+                    const query = {}
+                    query.routeId = routeId
+                    if (date) {
+                        query.date = date
+                    }
+
+                    console.log(query)
+
+                    const scheduleResponse = await db.Schedule.findAndCountAll({
+                        where: query,
+                        attributes:{
+                            exclude: ['createdAt', 'updatedAt']                            
+                        },
+                        raw: true
+                    })
+                    
+                    const routeResponse = await db.Route.findOne({
+                        where: {
+                            id: routeId
+                        },
+                        attributes: [
+                            'id'
+                        ],
+                        include: [
+                            { 
+                                model: db.Location, 
+                                as: 'fromLocation' ,
+                                attributes: [
+                                    'id',
+                                    'name',
+                                    'province'
+                                    
+                                ]
+                            },
+                            { 
+                                model: db.Location, 
+                                as: 'toLocation',
+                                attributes: [
+                                    'id',
+                                    'name',
+                                    'province'
+                                    
+                                ] 
+                            }
+                        ],
+                        raw: true,
+                        nest: true
+                    })
+                    resolve({
+                        success: scheduleResponse ? true : false,
+                        data: {
+                            scheduleData: scheduleResponse? scheduleResponse : null,
+                            routeData: routeResponse? routeResponse : null
+                        }
+                    })
+                }
+    
+                resolve({
+                    success: false,
+                    data: null
+                })
+            } else {
+                const response = await db.Schedule.findAll()
+                resolve({
+                    success: response? true : false,
+                    data: response? response : null
+                })
+            }            
         } catch (error) {
             reject(error)   
         }
