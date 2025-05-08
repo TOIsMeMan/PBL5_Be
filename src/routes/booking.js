88c6420f5fbfe8c67b0df9551e11769c3b08c7cd
Nguyễn Công Tuấn <147248673+TOIsMeMan@users.noreply.github.com>
2025-05-08 09:45:48 +0700

    update booking get all

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,8 +1,10 @@
 const bookingController = require('../controllers/booking')
+const verifyToken = require('../middleware/verifyToken')
 const router = require('express').Router()
 
 router.post('/detail', bookingController.getBookingByIdController)
-router.post('/get_all', bookingController.getAllBookingController)
 router.post('/', bookingController.sendBookingController)
+router.use(verifyToken)
+router.post('/get_all', bookingController.getAllBookingController)
 
 module.exports = router
